e001e84a7f4a8722f0c707eb7d0d0b31c9dbd6fd
Dacrol <jonas.ahrens@gmail.com>
2018-08-31 14:58:24 +0200

    Add time formater

--- public/main.js
+++ public/main.js
@@ -106,3 +106,13 @@ function nextPrev(prev = 1) {
 function switchViews () {
   $('#left, #right').toggleClass(['d-none', 'd-flex'])
 }
+
+function formatTime (secs) {
+  var sec_num = parseInt(secs, 10); // don't forget the second param
+  var minutes = Math.floor(sec_num / 60);
+  var seconds = sec_num - (minutes * 60);
+
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return minutes + ':' + seconds;
+}
