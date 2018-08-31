23e783cc5577d08fdffdbd509f1852be129ed3e2
Dacrol <jonas.ahrens@gmail.com>
2018-08-31 13:46:38 +0200

    Dynamically add songs to playlist

--- public/main.js
+++ public/main.js
@@ -1,3 +1,18 @@
+const songs = ['bad_apple', 'fires_at_midnight', 'devil_trigger']
+
+songs.forEach((song) => {
+  $('.songs').append(`<div class="song d-flex align-items-center pl-4 pr-4">
+  <div class="song-cover">
+    <img class="img-thumbnail" src="/${song}.jpg" alt="${titleCase(song)}">
+  </div>
+  <div class="flex-fill song-title pl-4">${titleCase(song)}</div>
+  <div class="play-icon">
+    <i class="fas fa-play-circle" aria-hidden="true"></i>
+  </div>
+  <audio class="audio_${song}" src="${song}.mp3" ></audio>
+</div>`)
+})
+
 $('.play-button').click(function (e) { 
   e.preventDefault()
   play()
@@ -21,4 +36,12 @@ function pause() {
 function toggleButtons() {
   $('.pause-button').toggle()
   $('.play-button').toggle()
+}
+
+function titleCase(str) {
+  var splitStr = str.replace(/_/g, ' ').toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  return splitStr.join(' '); 
 }
